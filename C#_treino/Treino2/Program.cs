af9540862dc7ccb670ed18f96420646c3ab9ad5a
AlanFC253 <ala253523@live.com>
2022-12-24 17:55:22 -0300

    Treino de C#

---
+++ C#_treino/Treino2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Treino2
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+
+            Console.WriteLine("Digita um numero ai");
+            int valor = int.Parse(Console.ReadLine());  
+
+            Console.WriteLine("o valor que vc digitou foi {0}",valor);
+        }
+    }
+}
